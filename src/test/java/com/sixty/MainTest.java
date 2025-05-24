f6ce71cadc2d2c7e2441e45f4e312c1420ed3436
sixty <1225207523@qq.com>
2025-05-24 18:27:34 +0800

    first commit

--- src/test/java/com/sixty/MainTest.java
+++ src/test/java/com/sixty/MainTest.java
@@ -5,13 +5,16 @@ import com.sixty.util.DataUtil;
 
 public class MainTest {
     public static void main(String[] args) {
-
         GroupInfo groupInfo = new GroupInfo();
-        groupInfo.setGroup(true);
+        groupInfo.setId(1L);
         groupInfo.setGroupId("111111");
+        groupInfo.setGroup(true);
         System.out.println(DataUtil.save(GroupInfo.class).insert(groupInfo));
         for (GroupInfo info : DataUtil.find(GroupInfo.class).execute()) {
-            DataUtil.del(GroupInfo.class).delete(info.getId()+11111);
+            System.out.println(info.getId());
+            System.out.println(info.getGroupId());
+            System.out.println(info.isGroup());
+            DataUtil.del(GroupInfo.class).delete(info.getId());
         }
 
     }
